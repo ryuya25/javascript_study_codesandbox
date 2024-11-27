99e976ec23652590a64e50fb73db114740bf5d8b
都筑輝泰（てるやす） <141887613+ryuya25@users.noreply.github.com>
2024-11-27 08:25:48 +0000

    Chapter46. Reactでのイベントやスタイルの扱い方を知る 完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 export const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px",
+    margin: 100,
+  };
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか</p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={contentStyle}>お元気ですか</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
