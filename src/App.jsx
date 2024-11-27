0468a8ec57b6b4db7f03e6c7038ff047d35427c2
都筑輝泰（てるやす） <141887613+ryuya25@users.noreply.github.com>
2024-11-27 09:02:55 +0000

    Chapter48. Stateを知る 完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfullMessage";
 
 export const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum((prev) => {
+      return prev + 1;
+    });
+    setNum((prev) => prev + 1);
+  };
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="green">元気ですよ！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
