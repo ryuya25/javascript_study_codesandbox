2269b65e3d99cda6b56f34d358958cddf0d0f531
都筑輝泰（てるやす） <141887613+ryuya25@users.noreply.github.com>
2024-11-27 08:46:35 +0000

    Chapter47. Propsを知る 完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import { ColorfulMessage } from "./components/ColorfullMessage";
+
 export const App = () => {
   const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px",
-    margin: 100,
-  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
-      <p style={contentStyle}>お元気ですか</p>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="green">元気ですよ！</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
---
+++ src/components/ColorfullMessage.jsx
@@ -0,0 +1,8 @@
+export const ColorfulMessage = ({ color, children }) => {
+  const contentStyleA = {
+    color,
+    fontSize: "18px",
+  };
+
+  return <p style={contentStyleA}>{children}</p>;
+};
